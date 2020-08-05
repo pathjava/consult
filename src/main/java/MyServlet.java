944389fee6001475e2671bbda5fbeee855eab594
Oleg K <severniyman@gmail.com>
2020-08-05 20:46:43 +0300

    consult

---
+++ src/main/java/MyServlet.java
@@ -0,0 +1,34 @@
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/myservlet")
+public class MyServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><head></head><body>Hello Servlet</body></html>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
