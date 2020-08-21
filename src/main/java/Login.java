45058fcc646b2092e23e0f3a35c5283a9dca4d20
Oleg K <severniyman@gmail.com>
2020-08-21 18:08:23 +0300

    consult

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -3,16 +3,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/login")
 public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
         if ("true".equals(req.getParameter("logout"))) {
             req.getSession().invalidate();
             resp.sendRedirect("/login");
-        }else
+        } else if (session.getAttribute("login") != null) {
+            resp.sendRedirect("/");
+        } else
             req.getRequestDispatcher("/login.jsp").forward(req, resp);
     }
 }
