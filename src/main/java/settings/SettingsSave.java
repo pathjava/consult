d4b1cf8744daafbd65be2d20421b686cfe8e7043
Oleg K <severniyman@gmail.com>
2020-08-24 19:52:01 +0300

    consult

--- src/main/java/settings/SettingsSave.java
+++ src/main/java/settings/SettingsSave.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @WebServlet("/settings-save")
 public class SettingsSave extends HttpServlet {
@@ -23,19 +22,33 @@ public class SettingsSave extends HttpServlet {
         if (days) {
             name = req.getParameter("selectMentor");
             List<String> params = new ArrayList<>(req.getParameterMap().keySet());
-            value = getMentorParam(params, req);
-
+            StringBuilder allParams = new StringBuilder();
+            for (int i = 0; i < params.size(); i++) {
+                if (params.get(i).startsWith("checkTrue")) {
+                    String day = req.getParameter(params.get(i));
+                    String time = req.getParameter(params.get(i + 1));
+                    String duration = req.getParameter(params.get(i + 2));
+                    if (time.isEmpty() || duration.isEmpty()) {
+                        req.setAttribute("error-description", "Время начала или продолжительность консультации не заданы!");
+                        req.getRequestDispatcher("/error.jsp").forward(req, resp);
+                        return;
+                    }
+                    allParams.append(day).append(";").append(time).append(";").append(duration).append("|");
+                }
+            }
+            allParams.setLength(allParams.length() - 1);
+            value = allParams.toString();
         } else {
             name = req.getParameter("name");
             value = req.getParameter("value");
 
             if (name == null || value == null) {
-                req.setAttribute("error-description", "Хакер? Отсутствуют обязательные параметры.");
+                req.setAttribute("error-description", "Хакер? Отсутствуют обязательные параметры!");
                 req.getRequestDispatcher("/error.jsp").forward(req, resp);
                 return;
             }
             if (name.isEmpty()) {
-                req.setAttribute("error-description", "Название параметра должно быть установлено.");
+                req.setAttribute("error-description", "Название параметра должно быть установлено!");
                 req.getRequestDispatcher("/error.jsp").forward(req, resp);
                 return;
             }
@@ -45,25 +58,10 @@ public class SettingsSave extends HttpServlet {
             DataBase.INSTANCE.settings.remove(name);
 
         if (!DataBase.INSTANCE.settings.put(new DataBase.Settings.Record(name, value))) {
-            req.setAttribute("error-description", "Не удалось добавить настройку. Вероятно, она уже существует.");
+            req.setAttribute("error-description", "Не удалось добавить настройку! Вероятно, она уже существует!");
             req.getRequestDispatcher("/error.jsp").forward(req, resp);
             return;
         }
         resp.sendRedirect("/settings-view");
-    }
-
-    private static String getMentorParam(List<String> params, HttpServletRequest req) {
-        StringBuilder allParams = new StringBuilder();
-
-        for (int i = 0; i < params.size(); i++) {
-            if (params.get(i).startsWith("checkTrue")) {
-                String day = req.getParameter(params.get(i));
-                String time = req.getParameter(params.get(i + 1));
-                String duration = req.getParameter(params.get(i + 2));
-                allParams.append(day).append("|").append(time).append("|").append(duration).append(";");
-            }
-        }
-        allParams.setLength(allParams.length() - 1);
-        return allParams.toString();
     }
 }
