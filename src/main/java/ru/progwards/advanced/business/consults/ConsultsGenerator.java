a79c6c0679698518e13a10cc68dba56eb3d9b938
Oleg K <severniyman@gmail.com>
2020-08-30 15:42:06 +0300

    consult

--- src/main/java/ru/progwards/advanced/business/consults/ConsultsGenerator.java
+++ src/main/java/ru/progwards/advanced/business/consults/ConsultsGenerator.java
@@ -9,13 +9,11 @@ import java.text.SimpleDateFormat;
 import java.time.*;
 import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
 import java.util.stream.Collectors;
 
 public class ConsultsGenerator {
 
     private static final long durationSlotTime = Utils.getTime(DataBase.INSTANCE.settings.findKey("SLOT_TIME").value);
-    private static long st;
 
     private void slotGenerator() throws IOException {
 
@@ -24,39 +22,39 @@ public class ConsultsGenerator {
                 .filter(schedule -> schedule.day_of_week == getCurrentDayOfWeek())
                 .collect(Collectors.toList());
 
-        if (schedules.size() > 0){
+        if (schedules.size() > 0) {
             for (DataBase.Schedule.Value schedule : schedules) {
-                long slotTime = schedule.start;
-                st = schedule.start; //TODO for test
-                System.out.println("start " + slotTime); //TODO for test
-                long endTime = endConsultationsTime(schedule.duration);
-                System.out.println("duration " + schedule.duration); //TODO for test
-                while (slotTime < endTime) {
+                long startConsultationsTime = getStartConsultationsTime(schedule.start);
+                long endConsultationsTime = startConsultationsTime + schedule.duration;
+                long slotTime = startConsultationsTime;
+
+                while (slotTime < endConsultationsTime) {
                     String mentorLogin = schedule.mentor;
-                    long startSlotTime = getStartSlotTime(slotTime);
                     String studentLogin = "";
                     String commentText = "";
                     DataBase.INSTANCE.consultations.put(new DataBase.Consultations.Consultation(mentorLogin,
-                            startSlotTime, durationSlotTime, studentLogin, commentText));
+                            slotTime, durationSlotTime, studentLogin, commentText));
+                    slotTime = getStartSlotTime(slotTime);
                 }
             }
         }
 
     }
 
-    private long endConsultationsTime(long duration) {
-        return 0;
+    private long getStartConsultationsTime(long start) {
+        LocalDateTime midnight = LocalDateTime.now().with(LocalTime.MIDNIGHT);
+        long midnightSeconds = Timestamp.valueOf(midnight).getTime();
+        return midnightSeconds + start;
     }
+
 
     private long getStartSlotTime(long slotTime) {
-        return 0;
+        return slotTime + durationSlotTime;
     }
 
-    private int getCurrentDayOfWeek(){
-        LocalDateTime now = LocalDateTime.now();
-        return now.getDayOfWeek().getValue();
+    private int getCurrentDayOfWeek() {
+        return LocalDateTime.now().getDayOfWeek().getValue();
     }
-
 
 
     public static void main(String[] args) {
@@ -69,35 +67,18 @@ public class ConsultsGenerator {
         }
         System.out.println(durationSlotTime);
 
-        System.out.println("--------------");
-        long now = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
-        System.out.println(now);
-        System.out.println("--------------");
-
+        System.out.println("-------время начала-------");
         SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd/HH:mm");
-        System.out.println(timeFormat.format(new Date(now)));
-        System.out.println("--------------");
-
-        LocalDateTime now1 = LocalDateTime.now();
-        System.out.println(now1.with(LocalTime.MIDNIGHT));
-
-        System.out.println("--------------");
-        LocalDate today = LocalDate.now(ZoneId.systemDefault());
-        LocalDateTime startOfDay = LocalDateTime.of(today, LocalTime.MIDNIGHT);
-        System.out.println(startOfDay);
-
-        System.out.println("--------------");
-        LocalDateTime now2 = LocalDateTime.now().with(LocalTime.MIDNIGHT);
-        long ll = Timestamp.valueOf(now2).getTime();
-        System.out.println(now2);
-        System.out.println(ll);
-
-        System.out.println("--------------");
-        LocalDateTime triggerTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(ll), ZoneId.systemDefault());
-        LocalDateTime triggerTime2 = LocalDateTime.ofInstant(Instant.ofEpochMilli(ll + st), ZoneId.systemDefault());
-
+        LocalDateTime midnight = LocalDateTime.now().with(LocalTime.MIDNIGHT);
+        long midnightSeconds = Timestamp.valueOf(midnight).getTime();
+        LocalDateTime triggerTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(midnightSeconds), ZoneId.systemDefault());
+//        LocalDateTime triggerTime2 = LocalDateTime.ofInstant(Instant.ofEpochMilli(midnightSeconds + st), ZoneId.systemDefault());
+        System.out.println(midnightSeconds);
         System.out.println(triggerTime);
-        System.out.println(triggerTime2);
+//        System.out.println(triggerTime2);
+        System.out.println(timeFormat.format(new Date(midnightSeconds)));
+//        System.out.println(timeFormat.format(new Date(midnightSeconds + st)));
+
     }
 
 }
